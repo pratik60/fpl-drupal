52eed559af4070a1165d2682a4dfe110c3ec0c21
pratik60 <pratik60@gmail.com>
2014-02-13 17:57:20 +0530

    first commit

---
+++ cl-part2.php
@@ -0,0 +1,69 @@
+<?php
+
+$gw = 2;
+$groups[0] = array('Atlanta','As Monacco','Elche','Cheslea');
+$gw2[0] = array('18763','10294','159599','306560');
+
+$groups[1] = array('Espanyol','Aston Villa','Paris Saint-Germain','Catania');
+$gw2[1] = array('43543','46','46103','96109');
+
+$groups[2] = array('Swansea','Athletico Madrid','Nurnber','Panathinaikos');
+$gw2[2] = array('8486','23093','63025','4491');
+
+$groups[3] = array('Athletic Bilabo','KR','Hull','Borussia');
+$gw2[3] = array('57410','239','179','6694');
+
+$groups[4] = array('Toulouse','Tottenham Hostpur','Malaga','Sporting');
+$gw2[4] = array('6887','178428','150487','58');
+
+$groups[5] = array('Hellas','VfB','FC Basel','Everton');
+$gw2[5] = array('9734','8251','44','283');
+
+$groups[6] = array('Hertha','Sevilla','Lazio','Montpellier');
+$gw2[6] = array('169','1588','41','550');
+
+$groups[7] = array('Feyenoord','Barcelona','Juventus','Bayern Munich');
+$gw2[7] = array('6015','1166','84','12438');
+
+echo '<table>';
+foreach ($groups as $group_key=>$group) {
+	$index = $group_key + 1;
+	echo  "<tr> <td> This is group " . $index . '</td></tr>';
+	foreach ($group as $team_id => $item) {
+    echo "<tr><td>" . $item . "</td><td>" .  score($gw2[$group_key][$team_id],$gw) . "</td></tr>";
+	}
+}
+echo '</table>';
+
+function score($team_id,$gw) {
+	$str2="http://fantasy.premierleague.com/entry/".$team_id."/event-history/".$gw;
+	$file=fopen($str2,"r") or die("Heavy load on FPL. Please try later");
+	while(!feof($file)) {
+		$str=fgets($file);
+		if(preg_match('/(<div class="ismSBValue ismSBPrimary">(.*))/', $str,$ar)) {
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2 = strip_tags($str2);
+			$str2=str_replace("pts","",$str2);
+			$temp1=(int)$str2;
+		}
+
+		if(preg_match('/(<dl class="ismDefList ismSBDefList">(.*))/', $str,$ar)) {
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2=fgets($file);
+			$str2 = strip_tags($str2);
+			$str2=str_replace("pts","",$str2);
+			$str2=str_replace("(-","",$str2);
+			$str2=str_replace(")","",$str2);
+			$temp2=(int)$str2;
+			$z=$temp1-$temp2;
+			return $z;
+		}
+	}
+}
+
+?>
