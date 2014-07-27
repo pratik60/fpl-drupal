d50a2b2069fa7c9755af154783c90723693e23b3
pratik60 <pratik60@gmail.com>
2014-07-27 14:35:34 +0530

    FPl theme

---
+++ sites/all/themes/fpl/fpl.js
@@ -0,0 +1,12 @@
+(function($) {
+  Drupal.behaviors.insight_client = {
+    attach: function(context, settings) {
+      // var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+      // $("#integration-types ul li a").each(function() {
+      //   if($(this).attr("href") === pgurl || $(this).attr("href") === '' )
+      //     alert($(this));
+      //   // $(this).closest('li').addClass("activeled");
+      // });
+    }
+  };
+})(jQuery);
